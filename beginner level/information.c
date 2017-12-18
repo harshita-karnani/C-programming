c4850b1e17a946c798fdb16de54b125cf4ad891a
harshita-karnani <34641377+harshita-karnani@users.noreply.github.com>
2017-12-18 14:14:39 +0530

    Create information.c

---
+++ beginner level/information.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+char name[25]="Sachin ramesh Tendulkar";
+char nickname[25]="Little Master";
+int runs_scored=18426;
+float batting_average=44.83;
+int ODI_matches=463;
+printf("%s\n",name);
+printf("%s\n",nickname);
+printf("%d\n",runs_scored);
+printf("%f\n",batting_average);
+printf("%d\n",ODI_matches);
+return 0;
+}
