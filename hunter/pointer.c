cec29e5b8e15f70ebd5c4c50166098967a64fff2
harshita-karnani <34641377+harshita-karnani@users.noreply.github.com>
2017-12-19 12:56:20 +0530

    Create pointer.c

---
+++ hunter/pointer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+char *s;
+int len,i;
+printf("\nENTER A STRING: ");
+gets(s);
+len=strlen(s);
+printf("\nTHE REVERSE OF THE STRING IS:");
+for(i=len;i>=0;i--)
+printf("%c",*(s+i));
+}
