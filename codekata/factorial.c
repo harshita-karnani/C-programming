1ed97ad1c326f43fa5c2b20a982c539cd4638941
harshita-karnani <34641377+harshita-karnani@users.noreply.github.com>
2018-03-12 11:59:14 +0530

    Create factorial.c

---
+++ codekata/factorial.c
@@ -0,0 +1,14 @@
+    #include <stdio.h>
+    int main(void) {
+    	int i,n,fact=1;
+    	scanf("%d",&n);
+    	i=1;
+    	while(i<=n)
+    	{
+    		fact=fact*i;
+    		i++;
+    	}
+    	printf("%d",fact);
+    	return 0;
+    }
+     
