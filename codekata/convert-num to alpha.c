a496a64d67874bb76aa2c88771e3d452391cca26
harshita-karnani <34641377+harshita-karnani@users.noreply.github.com>
2018-04-20 16:29:56 +0530

    Create convert-num to alpha.c

---
+++ codekata/convert-num to alpha.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(void) {
+	int num;
+	scanf("%d",&num);
+	switch(num)
+	{
+		case 1:printf("\none");
+			   break;
+		case 2:printf("\ntwo");
+			   break;
+		case 3:printf("\nthree");
+			   break;
+		case 4:printf("\nfour");
+			   break;
+        case 5:printf("\nfive");
+			   break;
+		case 6:printf("\nsix");
+			   break;
+		case 7:printf("\nseven");
+			   break;
+		case 8:printf("\neight");
+			   break;
+	    case 9:printf("\nnine");
+			   break;
+	    default:
+	    printf("print the number");
+	}
+	return 0;
+}
